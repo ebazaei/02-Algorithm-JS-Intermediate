6df507a4edb0c34723d227b03d37c77708a1025a
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-26 09:37:36 +0100

    tot 3

--- script.js
+++ script.js
@@ -13,16 +13,33 @@
 // Shift the Values
 // Given any array X, for example [2,1,6,4,-7], create an algorithm that shifts each number by one to the front. When your program is done the output for array [2,1,6,4,-7] should be [-7,4,6,1,2].
 
-let X = [2,1,6,4,-7]
-let arrayRev = [];
-let i = X.length-1;
-while (i >= 0)
-    {
-        arrayRev.push(X[i]);
-        i--;
-    }
-console.log(`${X} Reverse is = ${arrayRev}`);
+// let X = [2,1,6,4,-7]
+// let arrayRev = [];
+// let i = X.length-1;
+// while (i >= 0)
+//     {
+//         arrayRev.push(X[i]);
+//         i--;
+//     }
+// console.log(`${X} Reverse is = ${arrayRev}`);
 
+
+// 3. FizzBuzz
+// Create an algorithm that inserts the numbers from 1 to 135 into an array, while replacing the values that are divisible by 3 with the word "Fizz", the numbers that are divisible by 5 with the word "Buzz", and the numbers that are divisible with 3 and 5 with the word "FizzBuzz". Your output should look like [1,2,'Fizz',4,'Buzz',5,'Fizz',7,8,...]
+
+let newArray = [];
+
+for(var i = 1; i < 135 ; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        newArray.push("FizzBuzz");
+    }else if(i % 3 === 0){
+        newArray.push("Fizz");
+    }else if(i % 5 === 0){
+        newArray.push("Buzz");
+    }else{
+        newArray.push(i);
+    }
+}
 
 
 
